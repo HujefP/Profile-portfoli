60908747a00cad7e0906b7bb8dd483b3e92ebd1c
HujefP <patelhujef52@gmail.com>
2023-03-23 14:23:04 +0530

    js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+function user(){
+    let name = document.getElementById('name');
+    // let email = document.getElementById('email');
+    document.getElementById('username.innerText');
+    username.innerText='Welcome '+ name.value;
+    name.value='';
+    email.style.display='block';
+    name.style.display= 'none';
+}
+
+function usermail(){
+let mail= document.getElementById('email');
+ mail.value=''
+ mail.style.display= 'none'
+}
